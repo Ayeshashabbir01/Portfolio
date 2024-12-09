3a51c91fcff311915da33cea11db4f0d8fab42b5
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-12-09 23:33:45 +0500

    portfolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [
-#os.path.join(BASE_DIR ,'static')
-#]
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR ,'static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
